cf1e690c75d4cfe84ddc41d8c233df3e47f6a72d
Your Name <you@example.com>
2025-08-27 18:40:02 -0400

    subir repository

---
+++ App/calculate.py
@@ -0,0 +1,4 @@
+import math
+
+def hipotenusa_entera(a: int, b: int) -> int:
+    return round(math.sqrt(a**2 + b**2))
---
+++ test_sample.py
@@ -0,0 +1,4 @@
+from App.calculate import hipotenusa_entera
+
+def test_vista():
+    assert hipotenusa_entera(3, 4) == 5
